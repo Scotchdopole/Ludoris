c8e4135da1165ee347acf6e7bbd7fefb54b81b75
Scotchdopole <thonjirka@gmail.com>
2025-01-28 18:53:18 +0200

    updated

--- server/src/models/games.js
+++ server/src/models/games.js
@@ -12,11 +12,62 @@ module.exports = (sequelize, DataTypes) => {
 
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
 
+        //association
+        genres: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
 
+        //association
+        platforms: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+        engine: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
         
+        //association
+        developer: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+         //association
+        publisher: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+         //association
+        gameModes: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+        releaseDate: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+
+        price: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+
+        ytbTrailerLink: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+         
+
+
 
 
     })
