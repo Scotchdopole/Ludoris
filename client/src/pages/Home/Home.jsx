3903d79847a67ad8a1d607cc607ce60ddd6a3528
Scotchdopole <thonjirka@gmail.com>
2025-03-06 16:29:55 +0100

    Home page redesign

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,21 +7,22 @@ import NavBar from '../../components/NavBar/NavBar'
 
 export default function Home() {
   return (
+    <>
+   
     <div className='Home-body'>
-    
     <NavBar></NavBar>
+    
 
-    <div className="Home-Hero text-pop-up-tr">
-      <span className="Home-HeroText">
+    <div className="Home-Hero">
+      <span className="Home-HeroText text-pop-up-top">
         LUDORIS
       </span>
-
-
-      <span className="Home-HeroSubText " >
+      <span className="Home-HeroSubText" >
         Every game. Every detail. Instantly.
       </span>
-    </div>
+      </div>
     
     </div>
+    </>
   )
 }
